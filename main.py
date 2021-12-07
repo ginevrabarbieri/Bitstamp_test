0330c9e41bcfa1ff23784c0f26e1033d5255747f
DavideRossetto99 <davide.rossetto@student.h-farm.com>
2021-12-07 21:38:39 +0100

    I added help for each argument

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("crypto")
-parser.add_argument("currency")
-parser.add_argument("-sd","--specific_data")
+parser.add_argument("crypto", help="Specify the cryptocurrency code")
+parser.add_argument("currency", help="Specify the currency code")
+parser.add_argument("-sd","--specific_data", help="Specify which information you want to know")
 print(args.crypto, args.currency, args.specific_data)
