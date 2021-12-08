ea71f258b5231b83958191f7b6f6eb22dd927948
mattiacostacurta <mattia.costacurta@student.h-farm.com>
2021-12-08 19:41:46 +0100

    I've fixed Converted.py

--- Converter.py
+++ Converter.py
@@ -1,4 +1,6 @@
 def converter(price, default_currency, output_currency):
     c = CurrencyConverter()
+    default_currency = default_currency.upper()
+    output_currency = output_currency.upper()
     converted_price = c.convert(price, default_currency, output_currency)
     return converted_price
