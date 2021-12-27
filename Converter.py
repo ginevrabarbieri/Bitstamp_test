4892914c782f5d235094d2732c43bc097a2402c1
mattiacostacurta <mattia.costacurta@student.h-farm.com>
2021-12-27 14:55:59 +0100

    complete PEP8

--- Converter.py
+++ Converter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from currency_converter import CurrencyConverter
 
+
 def curr_converter(price, default_currency, output_currency):
     c = CurrencyConverter()
     default_currency = default_currency.upper()
@@ -8,8 +9,9 @@ def curr_converter(price, default_currency, output_currency):
     converted_price = c.convert(price, default_currency, output_currency)
     return converted_price
 
+
 def convert_table(output_currency):
     df = pd.read_csv('CryptoTable.csv')
     df['last'] = curr_converter(df._get_value(0, 'last'), "usd", output_currency)
     df['open'] = curr_converter(df._get_value(0, 'open'), "usd", output_currency)
-    df.to_csv (r'CryptoTable.csv', index = False, header=True)
+    df.to_csv(r'CryptoTable.csv', index=False, header=True)
