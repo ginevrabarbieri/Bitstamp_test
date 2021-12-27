f68c37b67c25931c9cb0ca711603d27d5e715df8
mattiacostacurta <mattia.costacurta@student.h-farm.com>
2021-12-27 14:38:45 +0100

    complete PEP8

--- bitstamp.py
+++ bitstamp.py
@@ -4,7 +4,9 @@ import json
 
 bitstamp_URL = 'https://www.bitstamp.net/api/v2/ticker/%s/'
 
+
 def get_price(currency="btceur"):
     r = requests.get(bitstamp_URL % currency)
     price = float(json.loads(r.text)['last'])
     return price
+    
