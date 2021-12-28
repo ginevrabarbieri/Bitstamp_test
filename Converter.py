4e933fd1c90eba912dc7ffdc34d476cb9e2c105a
mattiacostacurta <mattia.costacurta@student.h-farm.com>
2021-12-28 09:18:02 +0100

    fixed conflict when merged with costa

--- Converter.py
+++ Converter.py
@@ -2,7 +2,7 @@ import pandas as pd
 from currency_converter import CurrencyConverter
 
 
-def curr_converter(price, default_currency, output_currency):
+def converter(price, default_currency, output_currency):
     c = CurrencyConverter()
     default_currency = default_currency.upper()
     output_currency = output_currency.upper()
@@ -12,6 +12,6 @@ def curr_converter(price, default_currency, output_currency):
 
 def convert_table(output_currency):
     df = pd.read_csv('CryptoTable.csv')
-    df['last'] = curr_converter(df._get_value(0, 'last'), "usd", output_currency)
-    df['open'] = curr_converter(df._get_value(0, 'open'), "usd", output_currency)
+    df['last'] = converter(df._get_value(0, 'last'), "usd", output_currency)
+    df['open'] = converter(df._get_value(0, 'open'), "usd", output_currency)
     df.to_csv(r'CryptoTable.csv', index=False, header=True)
